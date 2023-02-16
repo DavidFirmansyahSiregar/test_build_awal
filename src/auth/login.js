616738af4eb3f88d888c95a93d87a24426b865dc
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-02-16 22:08:54 +0700

    logintanpaaxios

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,7 +1,53 @@
-import React from 'react'
+import React from "react";
+// import { useNavigate } from "react-router-dom";
+import { Button, Form, Input, } from "antd";
 
-export const login = () => {
-  return (
-    <div>login</div>
-  )
-}
+export const LoginPage = () => {
+    // const navigate = useNavigate();
+    
+    return (
+        <div>
+            <Form>
+            <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+                {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                },
+                {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                },
+            ]}
+        >
+            <Input 
+                placeholder="Input Email"
+            />
+        </Form.Item>
+        <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your password!',
+                },
+            ]}
+            hasFeedback
+        >
+            <Input.Password />
+        </Form.Item>
+            <Button
+                // onClick={() => {
+                //     localStorage.setItem("credentials", "value");
+                //     navigate("/dashboard");
+                // }}
+            >
+                L O G I N
+            </Button>
+            </Form>
+        </div>
+    );
+};
