c72e17d847d78986de02adb2cb5caf585994f2b6
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2023-02-20 14:34:55 +0700

    login

--- src/auth/login.js
+++ src/auth/login.js
@@ -1,53 +1,82 @@
 import React from "react";
 // import { useNavigate } from "react-router-dom";
-import { Button, Form, Input, } from "antd";
+import { Button, Form, Input } from "antd";
+// import { useState } from "react";
+// import { Axios } from "axios";
 
 export const LoginPage = () => {
-    // const navigate = useNavigate();
-    
-    return (
-        <div>
-            <Form>
-            <Form.Item
-            name="email"
-            label="E-mail"
-            rules={[
-                {
-                    type: 'email',
-                    message: 'The input is not valid E-mail!',
-                },
-                {
-                    required: true,
-                    message: 'Please input your E-mail!',
-                },
-            ]}
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <div>
+      <Form
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+          margin: 20,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!",
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
         >
-            <Input 
-                placeholder="Input Email"
-            />
+          <Input placeholder="Input Email" />
         </Form.Item>
         <Form.Item
-            name="password"
-            label="Password"
-            rules={[
-                {
-                    required: true,
-                    message: 'Please input your password!',
-                },
-            ]}
-            hasFeedback
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
         >
-            <Input.Password />
+          <Input.Password />
         </Form.Item>
-            <Button
-                // onClick={() => {
-                //     localStorage.setItem("credentials", "value");
-                //     navigate("/dashboard");
-                // }}
-            >
-                L O G I N
-            </Button>
-            </Form>
-        </div>
-    );
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button
+          // onClick={() => {
+          //     localStorage.setItem("credentials", "value");
+          //     navigate("/dashboard");
+          // }}
+          >
+            L O G I N
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
